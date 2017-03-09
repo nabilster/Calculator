ee5bf1780c7f6a0f29e14964c5df1ebc46ea602a
Nabil Sakran <nabilsakran@hotmail.com>
2017-03-09 02:55:31 -0500

    Update Calculator.java
    Added comments to understand how the calculator provides the MainActivity with its values. This includes how the values are store, the logic behind every operation, and how the final results are calculated and returned to the user.

--- src/com/karyog/calculator/Calculator.java
+++ src/com/karyog/calculator/Calculator.java
@@ -2,6 +2,8 @@ package com.karyog.calculator;
 
 import android.util.Log;
 
+// This is the main class of logic for the calculator. It takes input from MainActivity class, performs
+// the calculation and returns the correct answer to be displayed to the user.
 public class Calculator {
 	double num1;
 	String operator;
@@ -14,7 +16,10 @@ public class Calculator {
 
 	String finalDisplayText;
 	String finalDisplayMiniText;
-
+	
+	// Functions created to provide the current Display and MiniDisplay(shows equation to be calculated)
+	// strings to an external class.
+	// This is used by the MainActivity to update what is seen by the user in the Display and MiniDisplay
 	public String getFinalDisplayText() {
 		return finalDisplayText;
 	}
@@ -22,7 +27,7 @@ public class Calculator {
 	public String getFinalDisplayMiniText() {
 		return finalDisplayMiniText;
 	}
-
+	// Constructor used to create a new instance of the class and define variables of this instance
 	public Calculator() {
 		num1 = 0.0;
 		operator = "";
@@ -37,7 +42,9 @@ public class Calculator {
 		finalDisplayText = Double.valueOf(num1).toString();
 		finalDisplayMiniText = Double.valueOf(num1).toString();
 	}
-
+	// Takes in button values from MainActivity and determines what the character is by matching them to
+	// cases. When there is a match a certain function is executed depending on what the character is.
+	// If the character is not recognized it is considered misc and runs the miscOperatorEntered function.
 	public void calculate(String buttonText) {
 		currentDisplayMiniText = finalDisplayMiniText;
 		currentDisplayText = finalDisplayText;
@@ -94,7 +101,7 @@ public class Calculator {
 		}
 		standardizeDisplay();
 	}
-
+	// standardizes the displayed text so that innaccurate values cannot be entered. For ex. 3.333.3
 	private void standardizeDisplay() {
 		switch(finalDisplayText) {
 		case "0.0":
@@ -108,7 +115,8 @@ public class Calculator {
 			}
 		}
 	}
-
+	// When a period is entered the value is added to the finalDisplayText, where the display is updated
+	// with the addition of the period (when MainActivity calls FinalDisplayText() from this class).
 	private void periodEntered(String period) {
 		String existingDisplay = currentDisplayText;
 		Log.d("periodEntered", Boolean.valueOf(shouldClearText).toString());
@@ -122,7 +130,8 @@ public class Calculator {
 		System.out.println(". text = " + finalDisplayText);
 			
 	}
-
+	// When a number is entered the value is added to the finalDisplayText, where the display is updated
+	// with the addition of the new number (when MainActivity calls finalDisplayText() from this class).
 	private void numberEntered(String number) {
 		String existingDisplay = currentDisplayText;
 		Log.d("numberEntered", Boolean.valueOf(shouldClearText).toString());
@@ -143,7 +152,8 @@ public class Calculator {
 		finalDisplayText = existingDisplay + number;
 		System.out.println("1 text = " + finalDisplayText);
 	}
-
+	// When the toggle (+/-) button is pressed the finalDisplayText is updated to be the opposite value,
+	// negative or positive. This value will be used in further operations.
 	private void toggleSign() {
 		double number = Double.parseDouble(currentDisplayText);
 
@@ -152,7 +162,10 @@ public class Calculator {
 
 		finalDisplayText = Double.valueOf(number).toString();
 	}
-
+	// When the cases for clearing occurs, the finalDisplayText is cleared to 0. This means that the
+	// text in display is 0, but miniDisplay(the current full equation) is not cleared.
+	// In addition the current number value (num1) is reset to 0, as to keep display and calculation
+	// relationship consistent.
 	private void clearScreen(String clearButtonText) {
 		switch (clearButtonText) {
 		case "CE":
@@ -166,7 +179,8 @@ public class Calculator {
 			break;
 		}
 	}
-
+	// When B is pressed the last value in finalDisplayText is removed. This is done by getting the length
+	// and taking the substring of finalDisplayText with one less character.
 	private void backspaceEntered() {
 		Log.d("backspaceEntered", Boolean.valueOf(shouldClearText).toString());
 		if (shouldClearText == true) {
@@ -191,7 +205,9 @@ public class Calculator {
 		}
 
 	}
-
+	// When a known operator is entered, a calculation is done on the current value and the next value
+	// after the operator. The result of the calculation is saved in the "result" variable and equation
+	// is updated in miniDisplay
 	private void operatorEntered(String operation) {
 		Log.d("operatorEntered", "invoked");
 		System.out.println("current_display_text: " + currentDisplayText);
@@ -210,11 +226,15 @@ public class Calculator {
 		Log.d("operatorEntered", Boolean.valueOf(shouldClearText).toString());
 		shouldClearText = true;
 	}
-
+	// When a character is entered that is unknown a log message is printed and the character is ignored
+	// by the application
 	private void miscOperatorEntered(String operation) {
 		Log.d("miscOperatorEntered", operation);
 	}
-
+	// When the equals button is pressed the last number value entered is set to num2 and the result 
+	//(previous calculations when "=" not pressed) is used as the other value in the calculation.
+	// The result is then set to the finalDisplayText (showing on the display) and the MiniDisplayText 
+	// is cleared.
 	private void calculateResult() {
 		double num2 = Double.parseDouble(currentDisplayText);
 
@@ -229,7 +249,9 @@ public class Calculator {
 		finalDisplayMiniText = String.valueOf(num1);
 		shouldClearText = true;
 	}
-	
+	// Used when the user wants to find the rational value of the current number (1/currentDisplayText). 
+	// The result is cleared and the finalDisplayText (showing on display) is set to the rational value calculated. 
+	// The MiniDisplayText is reset to 0.
 	private void calculateReciprocal() {
 		if (currentDisplayText.equalsIgnoreCase("0.0")) {
 			finalDisplayText = "To Infinity and beyond!";
@@ -245,7 +267,11 @@ public class Calculator {
 		finalDisplayMiniText = String.valueOf(num1);
 		shouldClearText = true;
 	}
-
+	// Whenever a calculation is needed to occur from other methods, performOperation is called. This takes in the
+	// 2 numbers and the operation to be used between them.
+	//
+	// ** Note: This does not currently take into account order of operations when calculating as calculations are
+	//	    done right away when this is called.
 	private void performOperation(double num1, double num2, String op) {
 		switch (op) {
 		case "*":
@@ -265,7 +291,14 @@ public class Calculator {
 		}
 
 	}
-
+	// When any Memory operation is used the current value that is displayed (currentDisplayText) is
+	// manipulated as follows:
+	//
+	//		- MC clears the Memory
+	//		- MR recalls the current value in memory and puts it the the current display
+	//		- MS saves the current value in the display
+	//		- M+ adds the current value in display to the value already stored in memory
+	//		- M- subtracts the current value in display from the value already stored in memory
 	private void performMemoryOperation(String operation) {
 		switch (operation) {
 		case "MC":
