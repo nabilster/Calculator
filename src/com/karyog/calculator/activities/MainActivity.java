82fdf992425d9bf7ca9eac24f793d46f15ca2359
Nabil Sakran <nabilsakran@hotmail.com>
2017-03-09 00:38:45 -0500

    Update MainActivity.java
    Added comments to allow for clearer understanding of display adjustments and what is done by the application when a button is clicked.

--- src/com/karyog/calculator/activities/MainActivity.java
+++ src/com/karyog/calculator/activities/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+
+//sets the first activity (screen) that is viewed upon starting the calculator application
 public class MainActivity extends Activity {
 
 	Calculator calc;
@@ -23,12 +25,18 @@ public class MainActivity extends Activity {
 	@SuppressWarnings("deprecation")
 	@SuppressLint("NewApi")
 	@Override
+	
+	
+	// Upon activity creation gets the current displays height and width and adjusts the
+	// layout of the application according to a set default (prepare_screen_default function).
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		Display display = getWindowManager().getDefaultDisplay();
 		Point size = new Point();
 
+		// Gets current size of display using Android built in display method.
+		// Display may not exist in older devices. Catches and ignores potential error.
 		try {
 			display.getSize(size);
 		} catch (java.lang.NoSuchMethodError ignore) { // Older device
@@ -42,21 +50,33 @@ public class MainActivity extends Activity {
 		// Prepare screen based on width and height of the display
 		prepare_screen_default(screen_width, screen_height);
 
+		// Calculator object created to be used to calculate inputs and return answers
 		calc = new Calculator();
 	}
 
 	@SuppressLint("InflateParams")
 	private void prepare_screen_default(int screen_width, int screen_height) {
+		
+		// LayoutInflater (Built-In Android method) used to read the XML in res and create the view objects
+		// (TextView and Buttons) for the UI.
 		LayoutInflater li = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		RelativeLayout v = (RelativeLayout) li.inflate(R.layout.activity_main,
 				null);
 
-		// Divide the space equally for the controls
-		// width has the be divided into 5 as there are 5 columns.
-		// height has to be diveded by 7.5 -> 6 for buttons, 1 for the display,
-		// and .5 for the mini display that keeps track of operation sequence
-		// equals symbol will have twice the height, zero will have twice the
-		// width
+		// Creates the spacing of the screen for the display, miniDisplay, and all buttons,
+		//
+		// where the:
+		//
+		//		Width  = is divided into 5 for the 5 columns for buttons
+		//		Height = is divided by 7.5 for the 6 rows for buttons, 1 row for display, and
+		//				 0.5 for miniDisplay
+		//
+		// Special cases:
+		//
+		//		Equals Symbol = 2x the Height
+		//		Zero Button   = 2x the Width
+		//
+		
 		int width = screen_width;
 		int height = screen_height - (screen_height % 15);
 
@@ -65,110 +85,138 @@ public class MainActivity extends Activity {
 		TextView textView = (TextView) v.findViewById(R.id.display_result);
 		textView.setMinHeight(height / 15 * 2 + (screen_height % 15));
 
-		TextView textViewMini = (TextView) v
-				.findViewById(R.id.display_result_mini);
-
-		// this is mini display. actually height should be divided by 15
-		// (7.5 * 2). but for whatever reasons the display of the last row is
-		// not proper. adjustment is done here by reducing the height.
-		
+		// Manual reducing height of display for miniDisplay due to unknown spacing error.
+		TextView textViewMini = (TextView) v.findViewById(R.id.display_result_mini);
 		textViewMini.setMinHeight(height / 30);
 
 		Button zero = (Button) v.findViewById(R.id.zero);
 		zero.setMinWidth(width / 5 * 2);
 		zero.setMinHeight(height / 15 * 2);
+		
 		Button one = (Button) v.findViewById(R.id.one);
 		one.setMinWidth(width / 5);
 		one.setMinHeight(height / 15 * 2);
+		
 		Button two = (Button) v.findViewById(R.id.two);
 		two.setMinWidth(width / 5);
 		two.setMinHeight(height / 15 * 2);
+		
 		Button three = (Button) v.findViewById(R.id.three);
 		three.setMinWidth(width / 5);
 		three.setMinHeight(height / 15 * 2);
+		
 		Button four = (Button) v.findViewById(R.id.four);
 		four.setMinWidth(width / 5);
 		four.setMinHeight(height / 15 * 2);
+		
 		Button five = (Button) v.findViewById(R.id.five);
 		five.setMinWidth(width / 5);
 		five.setMinHeight(height / 15 * 2);
+		
 		Button six = (Button) v.findViewById(R.id.six);
 		six.setMinWidth(width / 5);
 		six.setMinHeight(height / 15 * 2);
+		
 		Button seven = (Button) v.findViewById(R.id.seven);
 		seven.setMinWidth(width / 5);
 		seven.setMinHeight(height / 15 * 2);
+		
 		Button eight = (Button) v.findViewById(R.id.eight);
 		eight.setMinWidth(width / 5);
 		eight.setMinHeight(height / 15 * 2);
+		
 		Button nine = (Button) v.findViewById(R.id.nine);
 		nine.setMinWidth(width / 5);
 		nine.setMinHeight(height / 15 * 2);
+		
 		Button Multiply = (Button) v.findViewById(R.id.Multiply);
 		Multiply.setMinWidth(width / 5);
 		Multiply.setMinHeight(height / 15 * 2);
+		
 		Button Divide = (Button) v.findViewById(R.id.Divide);
 		Divide.setMinWidth(width / 5);
 		Divide.setMinHeight(height / 15 * 2);
+		
 		Button Subtract = (Button) v.findViewById(R.id.Subtract);
 		Subtract.setMinWidth(width / 5);
 		Subtract.setMinHeight(height / 15 * 2);
+		
 		Button Add = (Button) v.findViewById(R.id.Add);
 		Add.setMinWidth(width / 5);
 		Add.setMinHeight(height / 15 * 2);
+		
 		Button Equals = (Button) v.findViewById(R.id.Equals);
 		Equals.setMinWidth(width / 5);
 		Equals.setMinHeight(height / 15 * 4);
+		
 		Button MC = (Button) v.findViewById(R.id.MC);
 		MC.setMinWidth(width / 5);
 		MC.setMinHeight(height / 15 * 2);
+		
 		Button MR = (Button) v.findViewById(R.id.MR);
 		MR.setMinWidth(width / 5);
 		MR.setMinHeight(height / 15 * 2);
+		
 		Button MS = (Button) v.findViewById(R.id.MS);
 		MS.setMinWidth(width / 5);
 		MS.setMinHeight(height / 15 * 2);
+		
 		Button MPlus = (Button) v.findViewById(R.id.MPlus);
 		MPlus.setMinWidth(width / 5);
 		MPlus.setMinHeight(height / 15 * 2);
+		
 		Button MMinus = (Button) v.findViewById(R.id.MMinus);
 		MMinus.setMinWidth(width / 5);
 		MMinus.setMinHeight(height / 15 * 2);
+		
 		Button BackSpace = (Button) v.findViewById(R.id.BackSpace);
 		BackSpace.setMinWidth(width / 5);
 		BackSpace.setMinHeight(height / 15 * 2);
+		
 		Button CE = (Button) v.findViewById(R.id.CE);
 		CE.setMinWidth(width / 5);
 		CE.setMinHeight(height / 15 * 2);
+		
 		Button Clear = (Button) v.findViewById(R.id.C);
 		Clear.setMinWidth(width / 5);
 		Clear.setMinHeight(height / 15 * 2);
+		
 		Button PlusMinus = (Button) v.findViewById(R.id.PlusMinus);
 		PlusMinus.setMinWidth(width / 5);
 		PlusMinus.setMinHeight(height / 15 * 2);
+		
 		Button Root = (Button) v.findViewById(R.id.Root);
 		Root.setMinWidth(width / 5);
 		Root.setMinHeight(height / 15 * 2);
+		
 		Button Mod = (Button) v.findViewById(R.id.Mod);
 		Mod.setMinWidth(width / 5);
 		Mod.setMinHeight(height / 15 * 2);
+		
 		Button OneByX = (Button) v.findViewById(R.id.OneByX);
 		OneByX.setMinWidth(width / 5);
 		OneByX.setMinHeight(height / 15 * 2);
-		Button DecimalSeparator = (Button) v
-				.findViewById(R.id.decimal_separator);
+		
+		Button DecimalSeparator = (Button) v.findViewById(R.id.decimal_separator);
 		DecimalSeparator.setMinWidth(width / 5);
 		DecimalSeparator.setMinHeight(height / 15 * 2);
-
+		
+		//reflects all the changes made by adjusting the display on activity creation
 		setContentView(v);
 	}
-
+	
+	// Allows for inflating the options menu at the bottom of all Android devices (bottom bar) just
+	// like the display was inflated/adjusted for all of the view elements
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	// Upon clicking a button it determines which button is clicked (by ID) and then sends what this button is to 
+	// the calculate function in the calculator class. The calculator class then takes this button as input.
+	// When a calculation is done the final answer returned by calcis shown on the display and 
+	// the full equation that was calculated is shown on the minidisplay.
 	public void buttonClick(View v) {
 		Button button = (Button) findViewById(v.getId());
 		String button_text = (String) button.getText();
